2a5582b2772221a172837f8683b30f334a4d5151
Serano Tannason Ng <stng@andrew.cmu.edu>
2018-09-16 07:38:58 -0400

    speaker recog

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,6 +12,9 @@ public class AudioManager : MonoBehaviour
     string _mic;
 
     public int SampleRate = 16000;
+    private string speakerRecognitionapiKey = "ec72d7fd829b4f25bf88d1be2c21c6e6";
+    private string speakerRecognitionEndpoint = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds=4db04ee6-97e5-4e90-af0c-eda22ec44e5f,f17c849d-4222-4bec-ba32-869ad4f7e239,c573830a-6249-4f75-9134-3fb3947b06e1&shortAudio=true";
+    private string firebaseEndpoint = "https://hellolens-5fbd4.firebaseio.com/";
 
     float[] audioData;
     int _totalRead = 0;
@@ -140,6 +143,141 @@ public class AudioManager : MonoBehaviour
 
             dataStream.Seek(0, SeekOrigin.Begin);
             dataStream.SetLength(0);
+        }
+    }
+
+    private IEnumerator GetSpeakerRecognitionTokenCoroutine(string key)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new InvalidOperationException("Speaker Recognition authorization key not set.");
+        }
+
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(speakerRecognitionEndpoint, string.Empty))
+        {
+            unityWebRequest.SetRequestHeader("Ocp-Apim-Subscription-Key", speakerRecognitionapiKey);
+            yield return unityWebRequest.SendWebRequest();
+
+            long responseCode = unityWebRequest.responseCode;
+
+            authorizationToken = unityWebRequest.downloadHandler.text;
+        }
+    }
+
+    public IEnumerator SpeakerRecognitionWithUnityNetworking()
+    {
+        WWWForm form = new WWWForm();
+        // form.AddField("timestamp", currentTime);
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(speakerRecognitionEndpoint, form))
+        {
+            unityWebRequest.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+            unityWebRequest.SetRequestHeader("Accept", "application/xml");
+
+            yield return unityWebRequest.SendWebRequest();
+
+            long responseCode = unityWebRequest.responseCode;
+
+            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Debug.Log(unityWebRequest.error);
+                yield return null;
+            }
+            else
+            {
+                string response = unityWebRequest.downloadHandler.text;
+                Debug.Log("Got response from coordination server: " + response);
+            }
+
+            // Parse out the response text from the returned Xml
+            string result = XElement.Parse(unityWebRequest.downloadHandler.text).Value;
+        }
+    }
+
+    public IEnumerator FirebaseWithUnityNetworking(string name, string text)
+    {
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(firebaseEndpoint + "counter.json"))
+        {
+            unityWebRequest.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+            unityWebRequest.SetRequestHeader("Accept", "application/xml");
+
+            yield return unityWebRequest.SendWebRequest();
+
+            long responseCode = unityWebRequest.responseCode;
+
+            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Debug.Log(unityWebRequest.error);
+                yield return null;
+            }
+            else
+            {
+                string response = unityWebRequest.downloadHandler.text;
+                Debug.Log("Got response from coordination server: " + response);
+            }
+
+            // Parse out the response text from the returned Xml
+            string result = XElement.Parse(unityWebRequest.downloadHandler.text).Value;
+            
+            int currentCounter = Int32.parse(result);
+            int newCounter = currentCounter + 1;
+            var currentTime = DateTime.Now.ToString("yyyy-MM-dd");
+
+            var newConversation = new Dictionary<string, Dictionary<string, string>>
+            {
+               { "conversation" + newCounter.ToString(), new Dictionary<string,string> 
+                    { { "timestamp", currentTime } ,
+                      { "text", conversation } } 
+                },
+            };
+
+            WWWForm form = new WWWForm();
+            form.AddField("timestamp", currentTime);
+            form.AddField("text", name + ": " + text + "\n");
+
+            using (UnityWebRequest unityWebRequest2 = UnityWebRequest.Post(firebaseEndpoint + "conversations.json", form))
+            {
+                unityWebRequest2.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+                unityWebRequest2.SetRequestHeader("Accept", "application/xml");
+
+                yield return unityWebRequest2.SendWebRequest();
+
+                responseCode = unityWebRequest2.responseCode;
+
+                if (unityWebRequest2.isNetworkError || unityWebRequest2.isHttpError)
+                {
+                    Debug.Log(unityWebRequest2.error);
+                    yield return null;
+                }
+                else
+                {
+                    string response = unityWebRequest2.downloadHandler.text;
+                    Debug.Log("Got response from coordination server: " + response);
+                }   
+            }
+
+            form = new WWWForm();
+            form.AddField("counter", newCounter);
+            
+            using (UnityWebRequest unityWebRequest3 = UnityWebRequest.Post(firebaseEndpoint + ".json", form))
+            {
+                unityWebRequest3.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+                unityWebRequest3.SetRequestHeader("Accept", "application/xml");
+
+                yield return unityWebRequest3.SendWebRequest();
+
+                responseCode = unityWebRequest3.responseCode;
+
+                if (unityWebRequest3.isNetworkError || unityWebRequest3.isHttpError)
+                {
+                    Debug.Log(unityWebRequest3.error);
+                    yield return null;
+                }
+                else
+                {
+                    string response = unityWebRequest3.downloadHandler.text;
+                    Debug.Log("Got response from coordination server: " + response);
+                }
+            }
         }
     }
 }
